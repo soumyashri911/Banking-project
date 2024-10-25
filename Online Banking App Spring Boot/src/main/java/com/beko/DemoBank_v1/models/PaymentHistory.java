552625970920937abeba22196c86be0a2afbb85a
Soumyashri <soumyashrikulkarni31@gmail.com>
2024-10-25 23:22:01 +0530

    Initial commit

---
+++ Online Banking App Spring Boot/src/main/java/com/beko/DemoBank_v1/models/PaymentHistory.java
@@ -0,0 +1,104 @@
+package com.beko.DemoBank_v1.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name ="v_payments")
+public class PaymentHistory {
+
+    @Id
+    @Column(name ="payment_id")
+    private int payment_id;
+    @Column(name ="account_id")
+    private int account_id;
+    @Column(name ="beneficiary")
+    private String beneficiary;
+    @Column(name ="beneficiary_acc_no")
+    private String beneficiary_acc_no;
+    @Column(name ="amount")
+    private double amount;
+    @Column(name ="reference_no")
+    private String reference_no;
+    @Column(name ="status")
+    private String status;
+    @Column(name ="reason_code")
+    private String reason_code;
+    @Column(name ="created_at")
+    private LocalDateTime created_at;
+
+    public int getPayment_id() {
+        return payment_id;
+    }
+
+    public void setPayment_id(int payment_id) {
+        this.payment_id = payment_id;
+    }
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getBeneficiary() {
+        return beneficiary;
+    }
+
+    public void setBeneficiary(String beneficiary) {
+        this.beneficiary = beneficiary;
+    }
+
+    public String getBeneficiary_acc_no() {
+        return beneficiary_acc_no;
+    }
+
+    public void setBeneficiary_acc_no(String beneficiary_acc_no) {
+        this.beneficiary_acc_no = beneficiary_acc_no;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getReference_no() {
+        return reference_no;
+    }
+
+    public void setReference_no(String reference_no) {
+        this.reference_no = reference_no;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getReason_code() {
+        return reason_code;
+    }
+
+    public void setReason_code(String reason_code) {
+        this.reason_code = reason_code;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+}
---
+++ Online Banking App Spring Boot/src/main/java/com/beko/DemoBank_v1/models/Transact.java
@@ -0,0 +1,109 @@
+package com.beko.DemoBank_v1.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transaction_history")
+public class Transact {
+    @Id
+    @GeneratedValue
+    @Column(name ="transaction_id")
+    private int transaction_id;
+    @Column(name ="account_id")
+    private int account_id;
+    @Column(name ="user_id")
+    private int user_id;
+    @Column(name ="transaction_type")
+    private String transaction_type;
+    @Column(name ="amount")
+    private double amount;
+    @Column(name ="source")
+    private String source;
+    @Column(name ="status")
+    private String status;
+    @Column(name ="reason_code")
+    private String reason_code;
+   
+
+    public int getTransaction_id() {
+        return transaction_id;
+    }
+
+    public void setTransaction_id(int transaction_id) {
+        this.transaction_id = transaction_id;
+    }
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getTransaction_type() {
+        return transaction_type;
+    }
+
+    public void setTransaction_type(String transaction_type) {
+        this.transaction_type = transaction_type;
+    }
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getReason_code() {
+        return reason_code;
+    }
+
+    public void setReason_code(String reason_code) {
+        this.reason_code = reason_code;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    private LocalDateTime created_at;
+
+
+}
